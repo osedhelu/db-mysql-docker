1cd5c00fef7c8aaa56b8a66ba321ac705b70bea2
Ubuntu <oscar.herrera@dis-global.com>
2024-07-26 22:05:40 +0000

    update(pagomovil): db_dump 02

--- db-dump.sh
+++ db-dump.sh
@@ -1,2 +1,11 @@
-docker exec -it mysql mysqldump -u admin -p ADMadm1234 laravel > ./backup.sql
+#!/bin/bash
+
+# Verifica si el contenedor está corriendo
+if ! docker ps | grep -q mysql; then
+	  echo "El contenedor MySQL no está corriendo."
+	    exit 1
+fi
+
+# Ejecuta el comando mysqldump dentro del contenedor
+docker exec -it mysql bash -c 'MYSQL_PWD=ADMadm1234 mysqldump -u admin laravel' > ./backup.sql
 
